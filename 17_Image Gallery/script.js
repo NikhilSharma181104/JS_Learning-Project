f34bc3d3998456968bef7cffbeccc3ecc26a09bd
Nikhil Sharma <nikhilsharma181104@gmail.com>
2026-01-03 14:21:38 +0530

    feat(image-gallery): Add horizontal scroll image gallery with navigation buttons and styling

---
+++ 17_Image Gallery/script.js
@@ -0,0 +1,18 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  scrollContainer.scrollLeft += evt.deltaY;
+  scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 300;
+});
+
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 300;
+});
